9846dc46d2c6338bb3ae9823c9b91e22c607c1f4
Frantisek Kobzik <git@franky.pub>
2023-12-06 21:02:19 +0100

    Add the day 3 placeholder for completion

---
+++ src/day_03.clj
@@ -0,0 +1,10 @@
+(ns day-03
+  (:require [clojure.string :as string]))
+
+
+;; Lessons
+;; - cider debugger
+;;  - just insert #break in front of form, where i'd like the debugger to stop
+;;  - or use #dbg:
+
+;; Part 1 works, but is not presentable
