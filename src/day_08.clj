1b7afa15ca0dc448175187be6dc444be41be2b43
Frantisek Kobzik <git@franky.pub>
2023-12-08 21:05:46 +0100

    Some cleanups in day 8

--- src/day_08.clj
+++ src/day_08.clj
@@ -23,7 +23,6 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)")
 
-
 (def example-input-3
   "LR
 
@@ -40,31 +39,27 @@ XXX = (XXX, XXX)")
 
 
 (def Targets
-  [:map {:closed? true} ["L" :string] ["R" :string]])
+  [:map {:closed? true} [\L :string] [\R :string]])
 
 ;; schema
 (def Input
   [:tuple
    :string                     ;; instructions
-   :string                     ;; start node
-   :string                     ;; target node
    [:map-of :string Targets]]) ;; node -> (L/R) -> target
 
 
-(mx/defn parse-node :- [:tuple :string [:map {:closed? true} ["L" :string] ["R" :string]]]
+(mx/defn parse-node :- [:tuple :string [:map {:closed? true} [\L :string] [\R :string]]]
   [node :- :string]
   (let [[start tgt-left tgt-right] (re-seq #"[A-Z0-9]+" node)]
-    [start {"L" tgt-left, "R" tgt-right}]))
+    [start {\L tgt-left, \R tgt-right}]))
 
 
 (mx/defn parse-input :- Input
   [input :- :string]
   (let [[instructions nodes-str] (string/split input #"\n\n")
         node-lines               (string/split-lines nodes-str)
-        start-node               (re-find #"[A-Z]+" (first node-lines))
-        tgt-node                 (re-find #"[A-Z]+" (last node-lines))
         nodes                    (map parse-node node-lines)]
-    [instructions start-node tgt-node (into {} nodes)]))
+    [instructions (into {} nodes)]))
 
 
 (defn traverse [node instruction nodes]
@@ -72,17 +67,14 @@ XXX = (XXX, XXX)")
 
 
 (defn part-1 [input-str]
-  (let [[instructions start-node tgt-node nodes] (parse-input input-str)]
+  (let [[instructions nodes] (parse-input input-str)]
     (loop [steps 0
-           node  "AAA"
-           insts (flatten (repeat (seq instructions)))]
+           node  "AAA"  ;; start node
+           insts (flatten (repeat (seq instructions)))  ;; infinite seq of instructions
+           ]
       (if (= node "ZZZ")
         steps
-        (recur (inc steps) (traverse node (str (first insts)) nodes) (rest insts))))))
-
-(take 10
-      (flatten
-       (repeat (seq "AB"))))
+        (recur (inc steps) (traverse node (first insts) nodes) (rest insts))))))
 
 
 (part-1 example-input)
